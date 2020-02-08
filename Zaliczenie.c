3603c4f225d4f6a0cbdac2a08edd011198da8f2d
patrykkobiela <57020326+patrykkobiela@users.noreply.github.com>
2020-02-08 16:49:47 +0100

    Create Zaliczenie.c

---
+++ Zaliczenie.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "SzyfrowanieCezara.h"
+
+int main(){
+    printf("SZYFROWANIE CEZARA wybierz 1 ");
+    int a;
+    scanf("%d", &a);
+    if (a=1){SzyfrowanieCezara();}
+    
+
+}
