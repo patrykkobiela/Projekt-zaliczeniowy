2e27c45ef35c6f688e47e802f40fa4fff7a427af
patrykkobiela <57020326+patrykkobiela@users.noreply.github.com>
2020-02-08 18:02:21 +0100

    Update Zaliczenie.c

--- Zaliczenie.c
+++ Zaliczenie.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "SzyfrowanieCezara.h"
+#include "OdszyfrowanieCezara.h"
 
 int main(){
-    printf("SZYFROWANIE CEZARA wybierz 1 ");
-    int a;
+    printf("SZYFROWANIE CEZARA wybierz 1 \n");
+    printf("ODSZYFROWANIE CEZARA wybierz 2 ");
+    int a; 
     scanf("%d", &a);
-    if (a=1){SzyfrowanieCezara();}
+    printf("%d \n", a);
+    switch(a){
+        case 1:
+        SzyfrowanieCezara();
+        break;
+        case 2:
+        OdszyfrowanieCezara();
+        break;
+        default:
+        printf("Wybor nie poprawny \n");
+    }
+   
     
 
 }
