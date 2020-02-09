34c66085a858f8f52dce5bc2854099ab0b9cbef8
patrykkobiela <57020326+patrykkobiela@users.noreply.github.com>
2020-02-09 16:46:45 +0000

    Update Zaliczenie.c

--- Zaliczenie.c
+++ Zaliczenie.c
@@ -2,13 +2,18 @@
 #include <stdlib.h>
 #include "SzyfrowanieCezara.h"
 #include "OdszyfrowanieCezara.h"
+#include "AlfabetMorsa.h"
+#include "OdwrotnyAlfabetMorsa.h"
 
 int main(){
-    printf("SZYFROWANIE CEZARA wybierz 1 \n");
-    printf("ODSZYFROWANIE CEZARA wybierz 2 ");
+    system("clear");
+    printf("\nMENU:\n\nSZYFROWANIE CEZARA wybierz 1 \n\n");
+    printf("ODSZYFROWANIE CEZARA wybierz 2\n\n");
+    printf("Alfabet Morsa wybierz 3\n\n");
+    printf("Odwrotny alfabet morsa wybierz 4\n\n");
     int a; 
     scanf("%d", &a);
-    printf("%d \n", a);
+    system("clear");
     switch(a){
         case 1:
         SzyfrowanieCezara();
@@ -16,9 +21,16 @@ int main(){
         case 2:
         OdszyfrowanieCezara();
         break;
+        case 3:
+        AlfabetMorsa();
+        break;
+        case 4:
+        OdwrotnyAlfabetMorsa();
+        break;
         default:
         printf("Wybor nie poprawny \n");
     }
+
    
     
 
